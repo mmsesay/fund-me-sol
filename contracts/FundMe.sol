bf9e54662eb8247d62abae0ce5eb4842aa0cf2d1
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-04-12 08:43:26 +0000

    Add: getPrice function to get the ether price to usd

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,39 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >0.6.6 <0.9.0;
+pragma solidity >0.8.0;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  // getRoundData and latestRoundData should both raise "No data present"
-  // if they do not have data to report, instead of returning unset values
-  // which could be misinterpreted as actual reported values.
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-}
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
   mapping(address => uint256) public addressToAmountFunded;
@@ -46,5 +15,12 @@ contract FundMe {
   function getVersion() public view returns (uint256) {
     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
     return priceFeed.version();
+  }
+
+  function getPrice() public view returns (uint256) {
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    (,int256 answer,,,) = priceFeed.latestRoundData();
+    return uint256(answer);
+    // 3,017.44772435 ETH to USD
   }
 }
